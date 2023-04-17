5eb164a8d93e5f42b6f11b62d487521fbbc41586
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 11:25:29 -0600

    Change pictures to png

---
+++ src/assests/data.js
@@ -0,0 +1,8 @@
+const data = [
+	{image: './Screenshots/Adam9.29.30AM.png', fact: 'fact', date: '2023-04-17', time: 'time'},
+	{image: 'image', fact: 'fact', date: '2023-04-17', time: 'time'},
+	
+
+]
+
+export default data
--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -1,7 +1,13 @@
 import './Fact.css'
+import data from '../../assests/data'
+import Blanche91936AM from '/Users/fayer/turing_work/4mod/ButDidYouKnow/src/assests/Screenshots/Brandon9.25.23 AM.png'
 
 export default function Fact() {
 	return(
-		<h1>Fact</h1>
+		<section>
+			<h1>Fact</h1>
+			{console.log(data[0].image)}
+			<img src={Blanche91936AM} alt='data0'/>
+		</section>
 	)
 }
