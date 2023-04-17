86c2ad5a6991cabcd0e09333c4a411eda4d15623
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 13:07:46 -0600

    Add fact image class

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -1,15 +1,15 @@
 import './Fact.css'
-
+import data from '../../assests/data'
 
 export default function Fact() {
 
-	const randomImageIndex = Math.floor(Math.random() * 30)
+	const randomImageIndex = Math.floor(Math.random() * 27)
 
 	return(
 		<section>
 			<h1>Fact</h1>
 			{console.log(data[0].image)}
-			<img src={data[0].image} alt={randomImageIndex}/>
+			<img classname='fact-image' src={data[randomImageIndex].image} alt={randomImageIndex}/>
 		</section>
 	)
 }
