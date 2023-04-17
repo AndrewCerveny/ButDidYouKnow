127c2bc349ae7d43be4fffdea89e978ad44ab68f
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 11:54:37 -0600

    Add random number generator

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -3,11 +3,14 @@ import data from '../../assests/data'
 import Blanche91936AM from '/Users/fayer/turing_work/4mod/ButDidYouKnow/src/assests/Screenshots/Brandon9.25.23 AM.png'
 
 export default function Fact() {
+
+	const randomImageIndex = Math.floor(Math.random() * 30)
+
 	return(
 		<section>
 			<h1>Fact</h1>
 			{console.log(data[0].image)}
-			<img src={data[0].image} alt='data0'/>
+			<img src={data[0].image} alt={randomImageIndex}/>
 		</section>
 	)
 }
