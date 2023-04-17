e914ed085764ea97bb447daf091b6444c7eb1059
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 13:57:23 -0600

    Add search bar

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -1,16 +1,17 @@
 import './Fact.css'
 import data from '../../assests/data'
 
-export default function Fact() {
+export default function Fact({value}) {
 
 	const randomImageIndex = Math.floor(Math.random() * 27)
 
 	return(
 		<section>
-			<h1>Fact</h1>
+			<h4>I'm sorry, I don't know about {value}</h4>
+			<h2>But did you know...</h2>
 			{console.log(data[0].image)}
-			<h2>{data[randomImageIndex].date}</h2>
-			<h2>{data[randomImageIndex].time}</h2>
+			<h2>On {data[randomImageIndex].date}</h2>
+			<h2>At {data[randomImageIndex].time}</h2>
 			<h2>{data[randomImageIndex].fact}</h2>
 			<img classname='fact-image' src={data[randomImageIndex].image} alt={randomImageIndex}/>
 		</section>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,23 @@
 import './SearchBar.css'
+import { useState } from 'react'
 
 export default function SearchBar() {
-	return(
-		<h1>SearchBar</h1>
+	const [searchInput, setSearchInput] = useState("")
+
+	const handleChange = (e) => {
+		e.preventDefault();
+		setSearchInput(e.target.value);
+	}
+
+	return (
+		<form>
+			<h2>Ask me anything!</h2>
+			<input
+				type="search"
+				placeholder="Search here"
+				onChange={handleChange}
+				value={searchInput} />
+				<button type="submit">Search</button>
+		</form>
 	)
 }
