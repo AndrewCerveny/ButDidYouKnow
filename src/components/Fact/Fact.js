436581fa77590252575788aec1a9b8fe37a4901b
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 14:30:13 -0600

    Add styling to Fact

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -8,14 +8,18 @@ export default function Fact() {
 	const randomImage = data[randomObjectIndex]
 	const randomFact = randomImage[randomFactIndex]
 
-	return(
+	return (
 		<section>
 			<h2>But did you know...</h2>
 			{console.log(data[0].image)}
-			<h2>On {randomImage.date}</h2>
-			<h2>At {randomImage.time}</h2>
-			<h2>{randomImage.randomFact}</h2>
-			<img classname='fact-image' src={randomImage.image} alt={randomImage}/>
+			<div className='fact-container'>
+				<img className='fact-image' src={randomImage.image} alt={randomImage} />
+				<div className='date-time-fact'>
+					<h2 className='fact-date'>On {randomImage.date}</h2>
+					<h2 className='fact-time'>at {randomImage.time}</h2>
+					<h2 className='fact-fact'>{randomImage.randomFact}</h2>
+				</div>
+			</div>
 		</section>
 	)
 }
