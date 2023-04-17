b667a5013d05c437110519eff48594bc2b963686
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 13:29:24 -0600

    Add date and time

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -9,6 +9,8 @@ export default function Fact() {
 		<section>
 			<h1>Fact</h1>
 			{console.log(data[0].image)}
+			<h2>{data[randomImageIndex].date}</h2>
+			<h2>{data[randomImageIndex].time}</h2>
 			<img classname='fact-image' src={data[randomImageIndex].image} alt={randomImageIndex}/>
 		</section>
 	)
