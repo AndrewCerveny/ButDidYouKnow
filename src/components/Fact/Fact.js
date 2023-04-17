b43a761000aa8ac55601a4606013d8c14b2b6b7d
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-17 13:47:34 -0600

    Add image date time and fact

--- src/components/Fact/Fact.js
+++ src/components/Fact/Fact.js
@@ -3,16 +3,19 @@ import data from '../../assests/data'
 
 export default function Fact() {
 
-	const randomImageIndex = Math.floor(Math.random() * 27)
+	const randomObjectIndex = Math.floor(Math.random() * 27)
+	const randomFactIndex = Math.floor(Math.random() * 2)
+	const randomImage = data[randomObjectIndex]
+	const randomFact = randomImage[randomFactIndex]
 
 	return(
 		<section>
 			<h1>Fact</h1>
 			{console.log(data[0].image)}
-			<h2>{data[randomImageIndex].date}</h2>
-			<h2>{data[randomImageIndex].time}</h2>
-			<h2>{data[randomImageIndex].fact}</h2>
-			<img classname='fact-image' src={data[randomImageIndex].image} alt={randomImageIndex}/>
+			<h2>{randomImage.date}</h2>
+			<h2>{randomImage.time}</h2>
+			<h2>{randomImage.randomFact}</h2>
+			<img classname='fact-image' src={randomImage.image} alt={randomImage}/>
 		</section>
 	)
 }
